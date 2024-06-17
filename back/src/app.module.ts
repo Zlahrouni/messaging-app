3d07315663c7a1f7c74d33f1223579ac0af8a9fb
Zlahrouni <ziad.lahrouni@gmail.com>
2024-06-17 14:52:41 +0200

    init graphql

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import {HealthController} from "./features/health/health.controller";
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import {UserResolver} from "./graphql/resolvers/UserResolver";
+import {HealthCheckResolver} from "./graphql/resolvers/HealthCheckResolver";
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.gql'
+    }),
+  ],
   controllers: [HealthController],
-  providers: [],
+  providers: [UserResolver, HealthCheckResolver],
 })
 export class AppModule {}
