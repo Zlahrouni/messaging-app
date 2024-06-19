8424f1852be5dc019c00a22de4163b69718ef38c
Sara Bevilacqua <sarabev6@gmail.com>
2024-06-19 14:06:03 +0200

    feat: send message

--- back/src/graphql/message/message.service.ts
+++ back/src/graphql/message/message.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Redis } from 'ioredis';
+import { MessageInput } from './dto/message.dto';
 
 @Injectable()
 export class MessageService {
@@ -12,9 +13,17 @@ export class MessageService {
     });
   }
 
-  async createMessage () {
+  async createMessage(messageDto: MessageInput) {
+    const messageId = Math.random().toString(36).substring(7);
+    this.redis.set(`messages: ${messageId}`, JSON.stringify(messageDto));
+    return messageDto;
   }
 
   async getMessages() {
+    const messages = await this.redis.keys('messages:*');
+    const messageData = await Promise.all(
+      messages.map((key) => this.redis.get(key)),
+    );
+    return messageData.map((message) => JSON.parse(message!));
   }
 }
