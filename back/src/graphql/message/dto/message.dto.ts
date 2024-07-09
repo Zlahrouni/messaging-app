0b809e20a886c301cbcfb0fd7ecf51ae1117ef8b
sabrina tamda <sabrinatamda5@gmail.com>
2024-07-09 13:22:21 +0200

    create message

--- back/src/graphql/message/dto/message.dto.ts
+++ back/src/graphql/message/dto/message.dto.ts
@@ -2,12 +2,16 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class MessageInput {
-  @Field()
-  content: string;
 
   @Field()
   senderId: string;
 
   @Field()
   receiverId: string;
+
+  @Field()
+  chatId: string;
+
+  @Field()
+  content: string;
 }
