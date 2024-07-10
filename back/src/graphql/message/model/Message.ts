05893e1f2d2bae314be4e10751979433c6c680cc
sabrina tamda <sabrinatamda5@gmail.com>
2024-07-10 11:34:09 +0200

    fix database

--- back/src/graphql/message/model/Message.ts
+++ back/src/graphql/message/model/Message.ts
@@ -1,22 +1,29 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
-
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
 @ObjectType()
 export class Message {
   @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Field(() => ID)
+  @Column()
   senderId: string;
 
   @Field(() => ID)
+  @Column()
   receiverId: string;
 
   @Field()
+  @Column()
   content: string;
 
   @Field()
+  @Column()
   chatId: string;
 
   @Field()
+  @Column({ default: () => 'CURRENT_TIMESTAMP'})
   createdAt: Date;
 }
