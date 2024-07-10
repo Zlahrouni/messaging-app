5ed3baded31deebfe270608bfa4a4b1ded8ff9a6
sabrina tamda <sabrinatamda5@gmail.com>
2024-07-10 17:42:10 +0200

    add test

--- back/src/graphql/message/message.resolver.spec.ts
+++ back/src/graphql/message/message.resolver.spec.ts
@@ -1,178 +1,255 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../user/user.service';
 import { MessageResolver } from './message.resolver';
 import { MessageService } from './message.service';
-import { MessageInput } from './dto/message.dto';
-import { ConflictException, NotFoundException } from '@nestjs/common';
+import { GetMessage, MessageInput } from './dto/message.dto';
 import { verifyOAuthToken } from '../module/auth';
+import { Message } from './model/Message';
+import { User } from '../user/model/User';
+import { NotFoundException } from '@nestjs/common';
 
-jest.mock('../module/auth', () => ({
-  verifyOAuthToken: jest.fn(),
-}));
+jest.mock('../module/auth');
+
+const mockMessageService = {
+  createMessage: jest.fn(),
+  getMessages: jest.fn(),
+};
+
+const mockUserService = {
+  getUserByEmail: jest.fn(),
+};
 
 describe('MessageResolver', () => {
   let resolver: MessageResolver;
-  //eslint-disable-next-line
-  let service: MessageService;
-
-  const messageServiceMock = {
-    createMessage: jest
-      .fn()
-      .mockImplementation((messageInput: MessageInput) => {
-        return {
-          ...messageInput,
-          id: '123456',
-        };
-      }),
-    getMessages: jest.fn(),
-  };
+  let messageService: jest.Mocked<MessageService>;
+  let userService: jest.Mocked<UserService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         MessageResolver,
-        { provide: MessageService, useValue: messageServiceMock },
+        { provide: MessageService, useValue: mockMessageService },
+        { provide: UserService, useValue: mockUserService },
       ],
     }).compile();
 
     resolver = module.get<MessageResolver>(MessageResolver);
-    service = module.get<MessageService>(MessageService);
+    messageService = module.get(MessageService);
+    userService = module.get(UserService);
   });
 
   describe('createMessage', () => {
-    const messageInputMock: MessageInput = {
-      senderId: 'senderId',
-      receiverId: 'receiverId',
-      chatId: 'chatId',
-      content: 'Hello, world!',
-      createdAt: new Date(),
-      token: 'token',
+    const messageInput: MessageInput = {
+      token: 'invalidToken',
+      receiveirEmail: 'receiver@example.com',
+      content: 'Hello!',
     };
 
-    it('should return an unauthorized response if the token is invalid', async () => {
+    it('should return 401 if token is invalid', async () => {
       (verifyOAuthToken as jest.Mock).mockResolvedValue(null);
 
-      const expectedMessage = {
+      const response = await resolver.createMessage(messageInput);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(messageInput.token);
+      expect(response).toEqual({
         code: 401,
         message: 'Unauthorized',
-        chat: null,
-      };
-
-      const result = await resolver.createMessage(messageInputMock);
-      expect(result).toEqual(expectedMessage);
-      expect(messageServiceMock.createMessage).not.toHaveBeenCalled();
+        messageCreated: null,
+      });
     });
 
-    it('should create a new message and return it if the token is valid', async () => {
-      const userEmailMock = 'user@example.com';
-      (verifyOAuthToken as jest.Mock).mockResolvedValue(userEmailMock);
+    it('should create a new message when the user has a valid token', async () => {
+      const senderEmail = 'sender@example.com';
+      const senderUser: User = {
+        email: senderEmail,
+        createdAt: new Date(),
+      };
+      const receiverUser: User = {
+        email: messageInput.receiveirEmail,
+        createdAt: new Date(),
+      };
 
-      const expectedMessage = {
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(senderEmail);
+      userService.getUserByEmail.mockResolvedValueOnce(senderUser);
+      userService.getUserByEmail.mockResolvedValueOnce(receiverUser);
+      messageService.createMessage.mockResolvedValue({
+        id: 'messageId',
+        content: 'Hello!',
+        senderEmail,
+        receiverEmail: messageInput.receiveirEmail,
+        createdAt: new Date(),
+        chatId: 'chatId',
+      } as Message);
+
+      const response = await resolver.createMessage(messageInput);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(messageInput.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(senderEmail);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(
+        messageInput.receiveirEmail,
+      );
+      expect(messageService.createMessage).toHaveBeenCalledWith(
+        senderUser.email,
+        receiverUser.email,
+        messageInput.content,
+      );
+      expect(response).toEqual({
         code: 200,
         message: 'Chat created successfully',
-        newMessage: {
-          ...messageInputMock,
-          id: '123456',
+        messageCreated: {
+          id: 'messageId',
+          content: 'Hello!',
+          senderEmail,
+          receiverEmail: messageInput.receiveirEmail,
+          createdAt: expect.any(Date),
+          chatId: 'chatId',
         },
-      };
-
-      const result = await resolver.createMessage(messageInputMock);
-      expect(result).toEqual(expectedMessage);
-      expect(messageServiceMock.createMessage).toHaveBeenCalledWith(
-        messageInputMock,
-      );
+      });
     });
 
-    it('should handle NotFoundException and return the appropriate response', async () => {
-      const notFoundException = new NotFoundException('Chat not found');
+    it('should return 404 if sender is not found', async () => {
+      const senderEmail = 'sender@example.com';
+      const receiverUser: User = {
+        email: messageInput.receiveirEmail,
+        createdAt: new Date(),
+      };
 
-      messageServiceMock.createMessage.mockRejectedValue(notFoundException);
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(senderEmail);
+      userService.getUserByEmail.mockResolvedValueOnce(null); // Sender not found
+      userService.getUserByEmail.mockResolvedValueOnce(receiverUser);
 
-      const expectedResult = {
+      const response = await resolver.createMessage(messageInput);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(messageInput.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(senderEmail);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(
+        messageInput.receiveirEmail,
+      );
+      expect(response).toEqual({
         code: 404,
-        message: notFoundException.message,
-        chat: null,
-      };
-
-      const result = await resolver.createMessage(messageInputMock);
-
-      expect(result).toEqual(expectedResult);
-      expect(messageServiceMock.createMessage).toHaveBeenCalledWith(
-        messageInputMock,
-      );
+        message: 'Sender not found',
+        messageCreated: null,
+      });
     });
 
-    it('should handle ConflictException and return the appropriate response', async () => {
-      const conflictException = new ConflictException('Chat already exists');
-
-      messageServiceMock.createMessage.mockRejectedValue(conflictException);
-
-      const expectedResult = {
-        code: 409,
-        message: conflictException.message,
-        chat: null,
+    it('should return 404 if receiver is not found', async () => {
+      const senderEmail = 'sender@example.com';
+      const senderUser: User = {
+        email: senderEmail,
+        createdAt: new Date(),
       };
 
-      const result = await resolver.createMessage(messageInputMock);
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(senderEmail);
+      userService.getUserByEmail.mockResolvedValueOnce(senderUser);
+      userService.getUserByEmail.mockResolvedValueOnce(null); // Receiver not found
 
-      expect(result).toEqual(expectedResult);
-      expect(messageServiceMock.createMessage).toHaveBeenCalledWith(
-        messageInputMock,
+      const response = await resolver.createMessage(messageInput);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(messageInput.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(senderEmail);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(
+        messageInput.receiveirEmail,
       );
-    });
-
-    it('should handle other errors and return the appropriate response', async () => {
-      const unknownError = new Error('Unknown error');
-
-      messageServiceMock.createMessage.mockRejectedValue(unknownError);
-
-      const expectedResult = {
-        code: 500,
-        message: 'Internal server error',
-        chat: null,
-      };
-
-      const result = await resolver.createMessage(messageInputMock);
-
-      expect(result).toEqual(expectedResult);
-      expect(messageServiceMock.createMessage).toHaveBeenCalledWith(
-        messageInputMock,
-      );
+      expect(response).toEqual({
+        code: 404,
+        message: 'Receiver not found',
+        messageCreated: null,
+      });
     });
   });
 
   describe('getMessages', () => {
-    it('should retrieve messages and return them', async () => {
-      const messages = [
+    const getMessage: GetMessage = {
+      token: 'validToken',
+      chatId: 'chatId',
+    };
+
+    it('should return 404 if the user is not authentified', async () => {
+      const email = 'user@example.com';
+
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(email);
+      userService.getUserByEmail.mockResolvedValue(null); // User not found
+
+      const response = await resolver.getMessages(getMessage);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(getMessage.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(email);
+      expect(response).toEqual({
+        code: 404,
+        message: 'User not found',
+        chats: [],
+      });
+    });
+
+    it('should retrieve messages successfully if the user is authentified', async () => {
+      const email = 'user@example.com';
+      const user = { email, createdAt: new Date() };
+      const messages: Message[] = [
         {
-          id: 'messageId1',
-          senderId: 'senderId1',
-          receiverId: 'receiverId1',
-          chatId: 'chatId1',
-          content: 'Hello, world!',
-          createdAt: new Date(),
-        },
-        {
-          id: 'messageId2',
-          senderId: 'senderId2',
-          receiverId: 'receiverId2',
-          chatId: 'chatId2',
-          content: 'Hi there!',
+          id: 'messageId',
+          chatId: 'chatId',
+          content: 'Hello!',
+          senderEmail: 'sender@example.com',
+          receiverEmail: 'receiver@example.com',
           createdAt: new Date(),
         },
       ];
 
-      messageServiceMock.getMessages.mockResolvedValue(messages);
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(email);
+      userService.getUserByEmail.mockResolvedValue(user);
+      messageService.getMessages.mockResolvedValue(messages);
 
-      const expectedResult = {
+      const response = await resolver.getMessages(getMessage);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(getMessage.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(email);
+      expect(messageService.getMessages).toHaveBeenCalledWith(
+        getMessage.chatId,
+        user.email,
+      );
+      expect(response).toEqual({
         code: 200,
         message: 'Messages retrieved successfully',
-        newmessage: messages,
-      };
+        messages: messages,
+      });
+    });
 
-      const result = await resolver.getMessages();
+    it('should handle NotFoundException thrown by messageService (messages not found) with a 404 code', async () => {
+      const email = 'user@example.com';
+      const user = { email, createdAt: new Date() };
 
-      expect(result).toEqual(expectedResult);
-      expect(messageServiceMock.getMessages).toHaveBeenCalled();
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(email);
+      userService.getUserByEmail.mockResolvedValue(user);
+      messageService.getMessages.mockRejectedValue(
+        new NotFoundException('Messages not found'),
+      );
+
+      const response = await resolver.getMessages(getMessage);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(getMessage.token);
+      expect(userService.getUserByEmail).toHaveBeenCalledWith(email);
+      expect(messageService.getMessages).toHaveBeenCalledWith(
+        getMessage.chatId,
+        user.email,
+      );
+      expect(response).toEqual({
+        code: 404,
+        message: 'Messages not found',
+        messages: [],
+      });
+    });
+
+    it('should handle a NotFoundExeption if a user is not part of this chat with a 404 code', async () => {
+      (verifyOAuthToken as jest.Mock).mockResolvedValue(null);
+
+      const response = await resolver.getMessages(getMessage);
+
+      expect(verifyOAuthToken).toHaveBeenCalledWith(getMessage.token);
+      expect(response).toEqual({
+        code: 404,
+        message: 'User not found',
+        chats: [],
+      });
     });
   });
 });
