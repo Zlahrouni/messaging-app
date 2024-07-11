61a4f3a3f092214ee6652d9a5887b5108920ebd8
sabrina tamda <sabrinatamda5@gmail.com>
2024-07-11 10:55:11 +0200

    add to db

--- back/src/graphql/chat/chat.service.ts
+++ back/src/graphql/chat/chat.service.ts
@@ -1,17 +1,29 @@
-import {ConflictException, forwardRef, Inject, Injectable, NotFoundException,} from '@nestjs/common';
+import {
+  ConflictException,
+  forwardRef,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import Redis from 'ioredis';
-import {v4 as uuidv4} from 'uuid';
-import {Chat} from './model/Chat';
-import {UserService} from '../user/user.service';
-import {MessageService} from "../message/message.service";
+import { v4 as uuidv4 } from 'uuid';
+import { Chat } from './model/Chat';
+import { UserService } from '../user/user.service';
+import { MessageService } from '../message/message.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ChatService {
   private redis: Redis;
 
   constructor(
-      private readonly userService: UserService, @Inject(forwardRef(() => MessageService))
-      private messageService: MessageService,) {
+    private readonly userService: UserService,
+    @Inject(forwardRef(() => MessageService))
+    private messageService: MessageService,
+    @InjectRepository(Chat)
+    private chatRepository: Repository<Chat>,
+  ) {
     this.redis = new Redis({
       host: 'localhost',
       port: 6379,
@@ -26,7 +38,7 @@ export class ChatService {
    * @throws NotFoundException - If the sender or receiver is not found.
    * @throws ConflictException - If the chat already exists.
    */
-  async createChat(senderEmail: string, receiverEmail: string) : Promise<Chat> {
+  async createChat(senderEmail: string, receiverEmail: string): Promise<Chat> {
     const sender = await this.userService.getUserByEmail(senderEmail);
     const receiver = await this.userService.getUserByEmail(receiverEmail);
 
@@ -47,6 +59,7 @@ export class ChatService {
       createdAt: new Date(),
     };
 
+    await this.chatRepository.save(newChat);
     await this.redis.set(`chats:${chatId}`, JSON.stringify(newChat));
 
     return newChat;
@@ -58,12 +71,27 @@ export class ChatService {
    */
   async getChats() {
     const chats = await this.redis.keys('chats:*');
-    const chatData = await Promise.all(chats.map((key) => this.redis.get(key)));
-    return chatData.map((chat) => {
-      const parsedChat = JSON.parse(chat!) as Chat;
-      parsedChat.createdAt = new Date(parsedChat.createdAt);
-      return parsedChat;
-    });
+
+    if (chats.length > 0) {
+      const chatData = await Promise.all(
+        chats.map((key) => this.redis.get(key)),
+      );
+      const myChats: Chat[] = chatData.map((chat) => {
+        const parsedChat = JSON.parse(chat!) as Chat;
+        parsedChat.createdAt = new Date(parsedChat.createdAt);
+        return parsedChat;
+      });
+
+      if (myChats.length === 0) {
+        throw new NotFoundException('No chats found');
+      }
+
+      return chats;
+    } else {
+      const chatsDB = await this.chatRepository.find();
+      console.log('savedChats', chatsDB);
+      return chatsDB;
+    }
   }
 
   /**
@@ -73,7 +101,15 @@ export class ChatService {
    */
   async getChatById(chatId: string) {
     const chat = await this.redis.get(`chats:${chatId}`);
-    return JSON.parse(chat!);
+
+    if (ChatService.length > 0) {
+      return JSON.parse(chat!);
+    } else {
+      const chatInDB = await this.chatRepository.findOne({
+        where: { id: chatId },
+      });
+      return chatInDB;
+    }
   }
 
   /**
@@ -82,7 +118,7 @@ export class ChatService {
    * @returns A list of chats.
    */
   async getChatsByEmail(email: string): Promise<Chat[]> {
-    const chats = await this.getChats();
+    const chats = (await this.getChats()) as Chat[];
     return chats.filter((chat) => chat.users.includes(email));
   }
 
@@ -92,16 +128,21 @@ export class ChatService {
    * @returns The chat or undefined.
    */
   async getChatByEmails(emails: string[]): Promise<Chat | undefined> {
-    const chats = await this.getChats();
-    return chats.find((chat) => chat.users.includes(emails[0]) && chat.users.includes(emails[1]));
+    const chats = (await this.getChats()) as Chat[];
+    return chats.find(
+      (chat) =>
+        chat.users.includes(emails[0]) && chat.users.includes(emails[1]),
+    );
   }
 
-  async getLastMessageOfChats(chats: Chat[]): Promise<Promise<{
-    createdAt: Date;
-    lastMessage: string;
-    id: string;
-    users: string[]
-  }>[]> {
+  async getLastMessageOfChats(chats: Chat[]): Promise<
+    Promise<{
+      createdAt: Date;
+      lastMessage: string;
+      id: string;
+      users: string[];
+    }>[]
+  > {
     return chats.map(async (chat) => {
       const messages = await this.messageService.getMessages(chat.id);
       const lastMessage = messages[messages.length - 1];
